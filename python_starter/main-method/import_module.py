84046513614574477791c5432edb8f38fc92355d
thapa-prashant <pthapa971@gmail.com>
2019-09-20 16:28:46 +0545

    python-master

---
+++ python_starter/main-method/import_module.py
@@ -0,0 +1,4 @@
+import module_example
+
+if __name__ == '__main__':
+    print("Hello World again from %s!" % __name__)
---
+++ python_starter/modules.py
@@ -0,0 +1,5 @@
+# A module is basically a file containing a set of functions to include in your application. There are core python modules, modules you can install using the pip package manager (including Django) as well as custom modules
+import classes
+
+if __name__ == '__main__':
+    print("Hello World again from")
